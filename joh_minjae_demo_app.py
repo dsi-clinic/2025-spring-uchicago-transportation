ac27a261b9270be98785d51bd2808e61d2f3e5df
mjayjoh <johminjae@gmail.com>
2025-04-03 17:53:30 -0500

    Fixed ruff-format

--- joh_minjae_demo_app.py
+++ joh_minjae_demo_app.py
@@ -1,26 +1,39 @@
-import streamlit as st
+'''
+Minjae Joh's Demo Streamlit App
+'''
+
 import pandas as pd
-import numpy as np
-from datetime import date, timedelta
-import string
-import time
+import streamlit as st
+
 
 @st.cache_data
 def get_data():
-    data_stopevents = pd.read_csv('data/ClinicDump-StopEvents.csv')
-    mean_durations = data_stopevents.groupby('routeName')['stopDurationSeconds'].mean()
+    '''
+    Get data for the app
+    '''
+    data_stopevents = pd.read_csv("data/ClinicDump-StopEvents.csv")
+    mean_durations = data_stopevents.groupby("routeName")["stopDurationSeconds"].mean()
     result = mean_durations.to_frame().T
     return result
 
+
 @st.fragment
 def show_mean_durations(data):
+    '''
+    Fragment showing the mean durations
+    '''
     st.header("Mean Stop Duration Seconds by Route")
     st.write("Below is a table showing mean stop duration for each route.")
     st.dataframe(data)
-    
+
+
 def main():
+    '''
+    Main Streamlit Function
+    '''
     data = get_data()
     show_mean_durations(data)
+
 
 if __name__ == "__main__":
     main()
