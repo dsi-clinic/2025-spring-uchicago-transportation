9f350cc9eeb4804898f1dc4504a1bf9647eb6cb6
mjayjoh <johminjae@gmail.com>
2025-04-03 17:54:07 -0500

    Fixed the docstrings to fit in one line.

--- joh_minjae_demo_app.py
+++ joh_minjae_demo_app.py
@@ -1,6 +1,4 @@
-'''
-Minjae Joh's Demo Streamlit App
-'''
+"""Minjae Joh's Demo Streamlit App"""
 
 import pandas as pd
 import streamlit as st
@@ -8,9 +6,7 @@ import streamlit as st
 
 @st.cache_data
 def get_data():
-    '''
-    Get data for the app
-    '''
+    """Get data for the app"""
     data_stopevents = pd.read_csv("data/ClinicDump-StopEvents.csv")
     mean_durations = data_stopevents.groupby("routeName")["stopDurationSeconds"].mean()
     result = mean_durations.to_frame().T
@@ -19,18 +15,14 @@ def get_data():
 
 @st.fragment
 def show_mean_durations(data):
-    '''
-    Fragment showing the mean durations
-    '''
+    """Fragment showing the mean durations"""
     st.header("Mean Stop Duration Seconds by Route")
     st.write("Below is a table showing mean stop duration for each route.")
     st.dataframe(data)
 
 
 def main():
-    '''
-    Main Streamlit Function
-    '''
+    """Main Streamlit Function"""
     data = get_data()
     show_mean_durations(data)
 
